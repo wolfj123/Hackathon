bf1b31d1356c7c60878b41846a894aef2efb0208
wolf123 <wolfj@post.bgu.ac.il>
2017-05-25 17:55:34 +0300

    Collider

--- src/JWclass.java
+++ src/JWclass.java
@@ -4,23 +4,41 @@ import java.util.Comparator;
 public class JWclass {
 
 	public static void main(String[] args) {
+		exc1();
+		
+		print("");
+		
+		exc3();
+	}
+	
+	
+	
+	public static void exc1(){
 		SecretHash hash = new SecretHash();
-		hash.setDifficulty(0);
+		hash.setDifficulty(1);
 		String s1 = hash.secretHash("hackathon");
 		String s2 = hash.secretHash("hackathon105");
-		String s3 = hash.secretHash("hackathon105aaa");
+		String s3 = hash.secretHash("hackathon105pizza");
+		
+		print(s1);print(s2);print(s3);
+	}
 	
-		print(s1);
+	public static void exc3(){
+		SecretHash hash = new SecretHash();
+		hash.setDifficulty(0);
+		String s2 = hash.secretHash("hackathon105");
+
 		print(s2);
-		print(s3);
+	}
+	
+	public static void exc4(){
+		SecretHash hash = new SecretHash();
+		hash.setDifficulty(0);
 		
-		ArrayList<String> list = new ArrayList<String>();
-		list.add(s1);list.add(s2);list.add(s3);
-		
-		list.sort(new StringComparator());
-		
-		
-		print(list.get(0));
+		String s1 = hash.secretHash("");
+		String s2 = hash.secretHash("");
+
+		print(s2);
 	}
 	
 	public static void print(String s){
